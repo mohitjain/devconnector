62f3a492ab025aa3be1c2409dd1d32475e327a43
Mohit Jain <jain.mohit27@gmail.com>
2020-07-05 18:26:11 +0530

    Basic Routing

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const connectDB = require("./config/db");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
 // Connect Database
-
 connectDB();
+
+// Define Routes
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 
 app.get("/", (req, res) => res.send("Api Running"));
 
